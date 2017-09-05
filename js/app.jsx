d99225f2a260fc190449a3b05bca0dd37d33b5ce
Łukasz Grząśko <lukasz.grzasko@hotmail.com>
2017-09-05 19:46:19 +0200

    prepared grid and reset

---
+++ js/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+import '../scss/styles.scss'
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    ReactDOM.render(
+        <p></p>,
+    document.getElementById('app')
+    )
+});
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: "./js/app.jsx",
+    output: { filename: "./js/out.js" },
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'stage-2', 'react']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+            }
+        ]
+    }
+}
