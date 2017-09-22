b0824e88ecc0124b9ff9166c8a8d145215319e4e
Łukasz Grząśko <lukasz.grzasko@hotmail.com>
2017-09-22 22:11:11 +0200

    sub-page sit-on-chair prepared for lrg screens

--- js/Components/Projects.jsx
+++ js/Components/Projects.jsx
@@ -16,14 +16,26 @@ class Projects extends React.Component {
                 <div className="row">
                     <div className="col-12">
                         <div className="first-picture-wrapper">
-                            <div className="first-picture">
-                                <img className="project-image" src="../images/example-mockup.png" alt="nazwa projektu"/>
-                                <div className="project-wrapper">
-                                    <div className="each-project">
-                                        <img className="showImage-1" src="images/project.png" alt="example-image"/>
-                                        <span>Zobacz</span>
-                                    </div>
+                            <div className="project-wrapper with-slider">
+                                <span className="slider-description">Dynamiczny slider</span>
+                                <div className="each-project">
+                                    <img className="showImage-1" src="images/responsive-slider.jpg" alt="responsive-slider"/>
                                 </div>
+                            </div>
+                            <div className="project-wrapper with-prizes">
+                                <span className="mobile-description">Responsywny layout </span>
+                                <div className="each-project">
+                                    <img className="showImage-2" src="images/responsive-mobile-view.png" alt="mobile-view"/>
+                                </div>
+                            </div>
+                            <div className="project-wrapper with-flex">
+                                <span className="flex-description">Flexbox layout</span>
+                                <div className="each-project">
+                                    <img className="showImage-3" src="images/flexbox-layout.png" alt="flexbox-layout"/>
+                                </div>
+                            </div>
+                            <div className="first-picture">
+                                <img className="project-image" src="images/showcase-top-sit.png" alt="sit-on-chair"/>
                             </div>
                         </div>
                     </div>
