cf4f18522f16a6cbe6af20f1cdec9c594ac7dac2
Łukasz Grząśko <lukasz.grzasko@hotmail.com>
2017-10-09 19:43:43 +0200

    fixed paths name and toggle menu

--- js/app.jsx
+++ js/app.jsx
@@ -6,14 +6,16 @@ import Main from './Components/Main.jsx';
 import Sitonchair from './Components/Sitonchair.jsx';
 import Nasaapi from './Components/Nasaapi.jsx';
 import Furrygame from './Components/Furrygame.jsx';
-import { Router, Route, IndexLink, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexLink, IndexRoute, hashHistory, useRouterHistory } from 'react-router'
+import { createHashHistory } from 'history'
 import '../scss/styles.scss'
 
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
 
 
 document.addEventListener('DOMContentLoaded', function(){
     ReactDOM.render(
-        <Router onUpdate={() => window.scrollTo(0, 0)} history={hashHistory}>
+        <Router onUpdate={() => window.scrollTo(0, 0)} history={appHistory}>
             <Route path="/" component={Template}>
                 <IndexRoute component={Main} />
                 <Route path="/sitonchair" component={Sitonchair} />
